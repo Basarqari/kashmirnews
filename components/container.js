72dfd3d8c2c0a43e1906df70debbf2300ea4b4ae
Bassam Ismail <skippednote@gmail.com>
2016-09-11 23:20:23 +0530

    Update title

--- components/container.js
+++ components/container.js
@@ -7,7 +7,7 @@ function container(tabListData) {
   <html lang="en">
   <head>
   <meta charset="UTF-8">
-  <title>Latest news from RK</title>
+  <title>Latest news from Kashmir</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
   * {
