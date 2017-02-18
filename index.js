db04c87afee8bf5452ae0a9e8acff6696858f8bf
Bassam Ismail <skippednote@gmail.com>
2017-02-18 19:57:57 +0530

    dev: Add API support.

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(compression());
 app.set('port', process.env.PORT || 3001);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/all', (req, res) => {
+  cacheGet('news')
+    .then((news) => res.json(news))
+})
+
 app.get('*', (req, res) => {
   cacheGet('news')
     .then(body => container(body))
