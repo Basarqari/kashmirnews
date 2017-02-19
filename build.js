e6e6a0035de2421b801953446fce8dd63a098d19
Bassam Ismail <skippednote@gmail.com>
2017-02-19 12:05:06 +0530

    dev: Add attributions and credits to extension.

--- build.js
+++ build.js
@@ -1,1 +1,159 @@
-!function(t){function e(r){if(n[r])return n[r].exports;var u=n[r]={i:r,l:!1,exports:{}};return t[r].call(u.exports,u,u.exports,e),u.l=!0,u.exports}var n={};return e.m=t,e.c=n,e.i=function(t){return t},e.d=function(t,n,r){e.o(t,n)||Object.defineProperty(t,n,{configurable:!1,enumerable:!0,get:r})},e.n=function(t){var n=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(n,"a",n),n},e.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},e.p="",e(e.s=2)}([function(t,e,n){"use strict";function r(t){return JSON.parse(t).reduce(function(t,e){var n=u(e),r="RK"===e.source?"checked":"",o='\n<div class="tab">\n  <input type="radio" id="tab__'+e.source+'" name="news" class="radio" '+r+' />\n  <label for="tab__'+e.source+'" class="label label__'+e.source+'">'+e.source+'</label>\n  <ul class="tab-list '+e.source+'">\n    '+n+"\n  </ul>\n</div>\n    ";return t+=o},"")}var u=n(1);t.exports=r},function(t,e,n){"use strict";function r(t){return t.data.reduce(function(e,n){var r="RK"===t.source?"http://www.risingkashmir.com/news/":"http://www.greaterkashmir.com",u=n.content?"<p>"+n.content+"</p>":"",o='\n<li class="tab-item">\n  <h3 class="tab-title">\n    <a href='+r+n.url+">"+n.title+"</a>\n  </h3>\n  "+u+"\n</li>\n    ";return e+=o},"")}t.exports=r},function(t,e,n){"use strict";function r(t){return t&&t.__esModule?t:{default:t}}function u(){fetch("http://kashmirnews.herokuapp.com/api/all").then(function(t){return t.json()}).then(function(t){return localStorage.setItem("news",JSON.stringify(t)),t}).then(function(){return(0,c.default)(localStorage.getItem("news"))}).then(function(t){return a.innerHTML=t})}var o=n(0),c=r(o),a=document.querySelector(".news");if(localStorage.getItem("news")){var s=(0,c.default)(localStorage.getItem("news"));a.innerHTML=s}u()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var tab = __webpack_require__(2);
+var footer = __webpack_require__(1);
+
+function tabList(tabListData) {
+  return JSON.parse(tabListData).reduce(function (acc, story) {
+    var tabItem = tab(story);
+    var checked = story.source === "RK" ? "checked" : "";
+    var attribution = story.source === "RK" ? "RisingKashmir" : "GreaterKashmir";
+    var tabWrapper = '\n<div class="tab">\n  <input type="radio" id="tab__' + story.source + '" name="news" class="radio" ' + checked + ' />\n  <label for="tab__' + story.source + '" class="label label__' + story.source + '">' + story.source + '</label>\n  <ul class="tab-list ' + story.source + '">\n    <h1 class="attribution">Source: <a href="http://' + attribution.toLowerCase() + '.com">' + attribution + '</a></h1>\n    ' + tabItem + '\n    ' + footer + '\n  </ul>\n</div>\n    ';
+    acc += tabWrapper;
+
+    return acc;
+  }, '');
+};
+
+module.exports = tabList;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var footer = "<footer>\n    <hr />\n    <p><a href=\"https://bassam.co\">Bassam Ismail</a> &middot; <a href=\"https://twitter.com/skippednote\">@skippednote</a></p>\n</footer>";
+
+module.exports = footer;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+function tab(story) {
+  return story.data.reduce(function (acc, item) {
+    var url = story.source === "RK" ? "http://www.risingkashmir.com/news/" : "http://www.greaterkashmir.com";
+    var content = item.content ? "<p>" + item.content + "</p>" : "";
+    var storyItem = "\n<li class=\"tab-item\">\n  <h3 class=\"tab-title\">\n    <a href=" + url + item.url + ">" + item.title + "</a>\n  </h3>\n  " + content + "\n</li>\n    ";
+    acc += storyItem;
+
+    return acc;
+  }, '');
+}
+
+module.exports = tab;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _tabList = __webpack_require__(0);
+
+var _tabList2 = _interopRequireDefault(_tabList);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var container = document.querySelector('.news');
+
+if (localStorage.getItem("news")) {
+    var markup = (0, _tabList2.default)(localStorage.getItem("news"));
+    container.innerHTML = markup;
+}
+getNews();
+
+function getNews() {
+    fetch("http://kashmirnews.herokuapp.com/api/all").then(function (blob) {
+        return blob.json();
+    }).then(function (json) {
+        localStorage.setItem("news", JSON.stringify(json));
+        return json;
+    }).then(function () {
+        return (0, _tabList2.default)(localStorage.getItem("news"));
+    }).then(function (markup) {
+        return container.innerHTML = markup;
+    });
+}
+
+/***/ })
+/******/ ]);
---
+++ components/footer.js
@@ -0,0 +1,6 @@
+var footer =`<footer>
+    <hr />
+    <p><a href="https://bassam.co">Bassam Ismail</a> &middot; <a href="https://twitter.com/skippednote">@skippednote</a></p>
+</footer>`
+
+module.exports = footer;
